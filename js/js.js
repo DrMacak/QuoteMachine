41de2680f6304883a5a68ad8f7e307351d865acb
Roman Macak <r.macak@seznam.cz>
2016-01-25 17:59:54 +0100

    Tweet in new window

--- js/js.js
+++ js/js.js
@@ -5,14 +5,12 @@ $(document).ready(function () {
    generateRandomQuote();
    console.log("test");
  });
-
-
 });
 
 function generateRandomQuote() {
   $.getJSON('json/fortunes.json', function (result) {
     var randomQuote = Math.floor(Math.random() * (result.length));
     $("#quotes").html("<h3>"+result[randomQuote].split("―").join("<br>-")+"</h3>");
-    $("#tweetContainer").html('<a class="twitter-button tweetbutton" href="https://twitter.com/intent/tweet?text='+result[randomQuote].substr(0,140)+'">Tweet</a>');
+    $("#tweetContainer").html('<a class="twitter-button tweetbutton" target="_blank" href="https://twitter.com/intent/tweet?text='+result[randomQuote].substr(0,140)+'">Tweet</a>');
   });
 }
