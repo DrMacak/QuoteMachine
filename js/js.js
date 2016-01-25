0d22de498684241d5c4096045b6ddd6b05cbc5b7
Roman Macak <r.macak@seznam.cz>
2016-01-25 15:05:26 +0100

    Added twitter button
    Tw button to share quotes.

--- js/js.js
+++ js/js.js
@@ -13,5 +13,6 @@ function generateRandomQuote() {
   $.getJSON('json/fortunes.json', function (result) {
     var randomQuote = Math.floor(Math.random() * (result.length));
     $("#quotes").html("<h3>"+result[randomQuote].split("―").join("<br>-")+"</h3>");
+    $("#tweetContainer").html('<a class="twitter-button tweetbutton" href="https://twitter.com/intent/tweet?text='+result[randomQuote].substr(0,140)+'">Tweet</a>');
   });
 }
